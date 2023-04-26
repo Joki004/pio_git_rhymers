60d255d2393260490c0e7e33c66a8be789226a4c
Joki004 <101185519+Joki004@users.noreply.github.com>
2023-04-26 22:57:02 +0200

    10. Dodanie gettera dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,7 +8,9 @@ public class DefaultCountingOutRhymer {
     private int[] NUMBERS = new int[ARRAY_SIZE];
 
     private int total = -1;
-
+    public int getTotal(){
+        return total;
+    }
     public void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
